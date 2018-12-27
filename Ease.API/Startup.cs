d36ec81342ea2d6a3befd8b30fea879fcd347408
Lincoln Lopes <lincolnlopes@msn.com>
2018-12-27 13:41:49 -0200

    * :Users:lincoln:Projects:Ease:Ease.API:config.json: Configuração de   conexão.
    * Startup.cs:
    * Ease.API.csproj:
    * :Users:lincoln:Projects:Ease:Ease.Domain:Entities:Customer.cs:

--- Ease.API/Startup.cs
+++ Ease.API/Startup.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ease.Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ease.API
 {
     public class Startup
     {
+        IConfiguration Configuration { get; set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("config.json");
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var conn = Configuration.GetConnectionString("EaseDB");
+            services.AddDbContext<EaseContext>(options => options.UseSqlServer(conn));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
